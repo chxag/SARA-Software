12eaa5d39618eb7c7a2a356d074f1a6497b56321
CiciTian <gongzimolan@163.com>
2024-02-05 21:38:49 +0000

    Added Grid JSON Object

---
+++ App/GridJSON.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import List
+import json
+
+@dataclass
+class Chair:
+    location: str
+    rotation: int
+    chairs: List[int]
+
+@dataclass
+class Dimension:
+    rows: int
+    columns: int
+
+@dataclass
+class Grid:
+    dimensions: Dimension
+    robot: str
+    stacks: List[Chair]
+
+    def __post_init__(self):
+        self.stacks = [Chair(**chair) for chair in self.stacks]
+        self.dimensions = Dimension(**self.dimensions)
--- App/SARA_server.py
+++ App/SARA_server.py
@@ -17,16 +17,30 @@ class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self) -> None:
         self.send_response(201)
 
+    # Grid JSON data will be sent via POST request
     def do_POST(self) -> None:
         content_length = int(self.headers.get('content-length', 0))
         post_data = self.rfile.read(content_length)
+        # Decode grid data encoded by 'utf-8'
         decoded_post_msg = post_data.decode('utf-8')
-        print(decoded_post_msg)
-
-        #print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                #str(self.path), str(self.headers), post_data.decode('utf-8'))
+        if content_length != 0:
+            print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), decoded_post_msg)
+            target_pose_x, target_pose_y, target_pose_z = compute_target_pose(decoded_post_msg)
+            target_orient_x, target_orient_y, target_orient_z, target_orient_w = compute_target_orient(decoded_post_msg)
             
+            execute_sara(target_pose_x, target_pose_y, target_pose_z, target_orient_x, target_orient_y, target_orient_z, target_orient_w)
+        else:
+            print("No grid data was received.\n")
         
+def compute_target_pose(decoded_post_msg):
+	return 0, 1, 2
+	
+def compute_target_orient(decoded_post_msg):
+	return 0, 1, 2, 3
+           
+def execute_sara(t_p_x, t_p_y, t_p_z, t_o_x, t_o_y, t_o_z, t_o_w):
+    print("SARA is on its way!\n")
 
 def server(port):
     httpd = socketserver.TCPServer(('', port), HTTPRequestHandler)
