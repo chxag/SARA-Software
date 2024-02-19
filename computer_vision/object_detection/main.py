052f5d587564fc1dbc6fd218c8e785197c6c5383
blackjack-boo <s2029304@ed.ac.uk>
2024-02-19 21:36:12 +0000

    Rename directory

--- computer_vision/object_detection/main.py
+++ computer_vision/object_detection/main.py
@@ -1,10 +1,10 @@
-from tiny_darknet.darknet import detect, load_net, load_meta
+from tiny_yolo.darknet import detect, load_net, load_meta
 import os
 
 directory = os.getcwd() + "/computer_vision/object_detection/"
-filepath_cfg = directory + "tiny_darknet/tiny.cfg"
-filepath_weights = directory + "tiny_darknet/tiny.weights"
-filepath_training_data = directory + "tiny_darknet/coco.data"
+filepath_cfg = directory + "tiny_yolo/tiny.cfg"
+filepath_weights = directory + "tiny_yolo/tiny.weights"
+filepath_training_data = directory + "tiny_yolo/coco.data"
 
 filepath_image = directory + "data/chair.jpg"
 
