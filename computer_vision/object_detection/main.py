75192871e9fdfe56cbb5c1eb13aae8818a5d85f2
blackjack-boo <s2029304@ed.ac.uk>
2024-02-19 21:33:22 +0000

    Filepath updates and settling on Tiny YOLO

--- computer_vision/object_detection/main.py
+++ computer_vision/object_detection/main.py
@@ -1,7 +1,15 @@
 from tiny_darknet.darknet import detect, load_net, load_meta
+import os
+
+directory = os.getcwd() + "/computer_vision/object_detection/"
+filepath_cfg = directory + "tiny_darknet/tiny.cfg"
+filepath_weights = directory + "tiny_darknet/tiny.weights"
+filepath_training_data = directory + "tiny_darknet/coco.data"
+
+filepath_image = directory + "data/chair.jpg"
 
 # Adapted from main method of Darknet
-net = load_net("tiny_darknet/tiny.cfg", "tiny_darknet/tiny.weights", 0)
-meta = load_meta("tiny_darknet/coco.data")
-r = detect(net, meta, "data/chair.jpg")
+net = load_net(filepath_cfg, filepath_weights, 0)
+meta = load_meta(filepath_training_data)
+r = detect(net, meta, filepath_image)
 print(r)
