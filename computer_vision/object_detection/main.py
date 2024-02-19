2227abc21e390f73e7e10607f3936b737e30918a
blackjack-boo <s2029304@ed.ac.uk>
2024-02-19 17:15:15 +0000

    Uncommitted work from Thursday

--- computer_vision/object_detection/main.py
+++ computer_vision/object_detection/main.py
@@ -1,4 +1,3 @@
-# from darknet.python.darknet import detect
+from tiny_darknet.darknet import detect, load_net, load_meta
 
-print("hello")
-# detect("cfg/yolov3.cfg", "yolov3.weights", "data/dog.jpg")
+net = load_net("tiny_darknet/tiny.cfg", "tiny_darknet/tiny.weights", 0)
