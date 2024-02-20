7c1c199013a9f69a0f192c79e490d80dada05d00
blackjack-boo <s2029304@ed.ac.uk>
2024-02-20 02:13:17 +0000

    Detection on multiple images with same net

--- computer_vision/object_detection/README.md
+++ computer_vision/object_detection/README.md
@@ -2,3 +2,6 @@
 YOLO v3 is a stronger object detector than Tiny YOLO (yolov2-tiny), however uses a large weights file that is too big to upload to a GitHub repository.
 To circumvent this, if using YOLO v3 simply download this file and place it in yolo/weights:
 https://pjreddie.com/media/files/yolov3.weights
+
+# Known issues
+Tiny YOLO does not work well on resized images, only those in the original size. For resized images and for better performance in general use YOLO v3.
--- computer_vision/object_detection/yolo/main.py
+++ computer_vision/object_detection/yolo/main.py
@@ -23,25 +23,39 @@ else:
     weights_file = "weights/yolov2-tiny.weights"
 training_data_file = "cfg/coco.data"
 
-# Create Object Detection System
 net = Detector(bytes(cfg_file, encoding="utf-8"), bytes(weights_file, encoding="utf-8"), 0, bytes(training_data_file, encoding="utf-8"))
 
-img = cv2.imread('data/chair.jpg')
-if RESIZE:
-    img = cv2.resize(img, (300, 300))  # Resize to 300*300 if desired
-img_darknet = Image(img)
+def prepare_image(filepath="data/chair.jpg"):
+    """Reads in the image at the supplied filepath as a cv2 image."""
+    img = cv2.imread(filepath)
+    if RESIZE:
+        img = cv2.resize(img, (300, 300))  # Resize to 300*300 if desired
+    return img
 
-results = net.detect(img_darknet)
-    
-for category, score, bounds in results:
-    if category == "chair":
-        x, y, w, h = bounds
+def detect_chairs(filepath="data/chair.jpg"):
+    """Detects chairs in the image at the supplied filepath in a returned list of bounding boxes."""
+    img = prepare_image(filepath)
+    results = net.detect(Image(img))
+    boxes = []
+    for category, score, bounds in results:
+        if category == "chair":
+            x, y, w, h = bounds
+            boxes.append([x, y, w, h])
+    return boxes
 
-        # Visualise bounding box around chairs
+def visualisation(filepath="data/chair.jpg", writeto="detected.jpg"):
+    """Produces an object detection visualisation and stores the result to an external file."""
+    img = prepare_image(filepath)
+    boxes = detect_chairs(filepath)
+    for box in boxes:
+        x, y, w, h = box
         thickness = 5 if RESIZE else 10
         font_scale = w / 100
         font_thickness = 1 if RESIZE else 5
         cv2.rectangle(img, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), BLUE, thickness=thickness)
         cv2.putText(img, "chair", (int(x),int(y)), cv2.FONT_HERSHEY_PLAIN, font_scale, BLUE, font_thickness)
+    cv2.imwrite(writeto, img)
 
-cv2.imwrite("detected.png", img)
+# Test Outputs
+visualisation()
+visualisation("data/chairs.jpg", "detected_multiple.jpg")
