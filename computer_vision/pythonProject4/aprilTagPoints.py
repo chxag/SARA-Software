f0490d3834e74bcf683bf487e28ce07f10dfbb98
Leo <89193836+Hejaii@users.noreply.github.com>
2024-02-22 12:50:24 +0000

    Add files via upload
    Orientation detection using April tags

---
+++ computer_vision/pythonProject4/aprilTagPoints.py
@@ -0,0 +1,32 @@
+import apriltag
+import cv2
+
+def detect_apriltag(image_path):
+    # load the input image and convert it to grayscale
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # define the AprilTags detector options and then detect the AprilTags
+    options = apriltag.DetectorOptions(families="tag36h11")
+    detector = apriltag.Detector(options)
+    results = detector.detect(gray)
+    print("[INFO] {} total AprilTags detected".format(len(results)))
+
+    # list to store coordinates of detected AprilTags
+    apriltag_coordinates = []
+
+    # loop over the AprilTag detection results
+    for r in results:
+        # extract the bounding box (x, y)-coordinates for the AprilTag
+        # and convert each of the (x, y)-coordinate pairs to integers
+        (ptA, ptB, ptC, ptD) = r.corners
+        ptB = (int(ptB[0]), int(ptB[1]))
+        ptC = (int(ptC[0]), int(ptC[1]))
+        ptD = (int(ptD[0]), int(ptD[1]))
+        ptA = (int(ptA[0]), int(ptA[1]))
+
+        # add the coordinates to the list
+        apriltag_coordinates.append((ptA, ptB, ptC, ptD))
+
+    return apriltag_coordinates
+
